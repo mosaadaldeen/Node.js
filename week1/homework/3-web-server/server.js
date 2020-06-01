ce3189f7053b3c11b604574fb3ce5f2571cd3209
mosaadaldeen <mohammadsd97@gmail.com>
2020-06-01 15:16:56 +0200

    fixed the repeated code

--- week1/homework/3-web-server/server.js
+++ week1/homework/3-web-server/server.js
@@ -3,25 +3,21 @@ var fs = require('fs');
 
 //create a server
 let server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        fs.readFile('index.html', (err, data) => {
-            res.writeHead(200, { 'Content-Type': 'text/html', 'Content-Length': data.length });
-            res.write(data);
-            res.end();
-        });
-    } else if (req.url === '/script.js') {
-        fs.readFile('script.js', (err, data) => {
-            res.writeHead(200, { 'Content-Type': 'text/JavaScript', 'Content-Length': data.length });
-            res.write(data);
-            res.end();
-        });
-    } else if (req.url === '/style.css') {
-        fs.readFile('style.css', (err, data) => {
-            res.writeHead(200, { 'Content-Type': 'text/css', 'Content-Length': data.length });
-            res.write(data);
-            res.end();
-        });
-    }
+    if (req.url === '/') readFile(res, 'index.html', 'text/html');
+    if (req.url === '/script.js') readFile(res, 'script.js', 'text/javascript');
+    if (req.url === '/style.css') readFile(res, 'style.css', 'text/css');
 });
+
+function readFile(res, file, contentType) {
+    fs.readFile(file, (err, data) => {
+        if (err) throw err;
+        res.writeHead(200, {
+            'Content-Type': contentType,
+            'Content-Length': data.length
+        });
+        res.write(data);
+        res.end();
+    });
+}
 
 server.listen(3000);
